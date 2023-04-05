f7bb84bc3edf2f0a301eba7f5f524b64312e75f0
Justus Dinera <frieden@freie-wahrheit.de>
2023-04-05 22:22:41 +0200

    kb2040 try

--- keyboards/crkbd/keymaps/dinera/config.h
+++ keyboards/crkbd/keymaps/dinera/config.h
@@ -43,8 +43,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /* Select hand configuration */
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+//#define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 
--- keyboards/crkbd/keymaps/dinera/rules.mk
+++ keyboards/crkbd/keymaps/dinera/rules.mk
@@ -1,16 +1,35 @@
+### Mouse
 MOUSEKEY_ENABLE     = no      # Mouse keys
-#RGBLIGHT_ENABLE     = yes     # Enable WS2812 RGB underlight.
+
+### RGB LEDs
+#RGB_MATRIX_CUSTOM_USER = yes
 RGB_MATRIX_ENABLE   = yes		  # Enable RGB matrix
 RGB_MATRIX_DRIVER   = WS2812    # Enable WS2812 for RGB Matrix
+#RGBLIGHT_ENABLE     = yes     # Enable WS2812 RGB underlight.
+
+### Vial settings
 VIA_ENABLE          = yes     # Enable VIA
 VIAL_ENABLE         = yes     # Enable VIAL
+
+### USB HID
 RAW_ENABLE          = yes 	  # Enable RAW HID
-OLED_ENABLE         = no
+
+### OLED
+#OLED_ENABLE         = yes
 #OLED_DRIVER         = SSD1306
+
+### Rotary Encoder
+ENCODER_ENABLE 		= yes        # Activate Encoder
+ENCODER_MAP_ENABLE 	= yes
+
+### Misc
 EXTRAKEY_ENABLE     = yes
 LTO_ENABLE          = yes
 QMK_SETTINGS        = no
 COMBO_ENABLE        = no
-#CONVERT_TO			= kb2040
-ENCODER_ENABLE = yes        # Activate Encoder
-ENCODER_MAP_ENABLE = yes
+
+### KB2040
+#PIN_COMPATIBLE = promicro
+CONVERT_TO			= kb2040 # Convert Code to Adafruit KB2040
+#MCU = kb2040
+#BOOTLOADER = rp2040
