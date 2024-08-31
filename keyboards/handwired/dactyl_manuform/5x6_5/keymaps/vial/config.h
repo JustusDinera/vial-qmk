24a2a83c4b19cfffa2365c07b4f91d495f29e7bf
Justus Dinera <frieden@freie-wahrheit.de>
2024-08-31 18:34:40 +0200

    bugfix

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/vial/config.h
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/vial/config.h
@@ -8,8 +8,25 @@
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
+#define MIDI_ADVANCED
+
+#define MASTER_LEFT
+
+#define DEBOUNCE 5
+#define ENCODERS_PAD_A { B2 }   //Port encoder left
+#define ENCODERS_PAD_B { B4 }
+#define ENCODERS_PAD_A_RIGHT { B4 } //Port encoder right
+#define ENCODERS_PAD_B_RIGHT { B2 }
+
+// reset behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
+
 // Vial features
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 20
 #define VIAL_KEY_OVERRIDE_ENTRIES 8
 #define VIAL_TAP_DANCE_ENTRIES 8
 #define VIAL_COMBO_ENTRIES 8
--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/vial/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/vial/rules.mk
@@ -4,8 +4,17 @@ VIAL_ENABLE = yes
 LTO_ENABLE = yes
 
 QMK_SETTINGS = no          # Vial tab
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+
+MIDI_ENABLE = yes
+RAW_ENABLE = yes
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+#PIN_COMPATIBLE = promicro
+#CONVERT_TO = rp2040
 
 DYNAMIC_MACRO_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
